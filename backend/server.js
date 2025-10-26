c7e9df0212aac74d08fd20d16691485bf0875ed1
ayush99336 <ayushkaushikhere@gmail.com>
2025-10-26 10:11:33 +0530

    - env for socket in useSocket, - added cors origin for frontend deployement in server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://3-patti-nu.vercel.app/"],
     methods: ["GET", "POST"],
   },
 });
